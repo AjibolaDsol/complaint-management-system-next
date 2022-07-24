f8f0e0e468724512b1852d1557af1933897cf384
AjibolaDsol <ajibolaDsol@gmail.com>
2022-07-24 01:10:36 +0100

    installed firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRUvgQmV98d4XFFVUy-Nc7YGZUjz4COPI",
+  authDomain: "complaint-management-sys-584e9.firebaseapp.com",
+  projectId: "complaint-management-sys-584e9",
+  storageBucket: "complaint-management-sys-584e9.appspot.com",
+  messagingSenderId: "88164443797",
+  appId: "1:88164443797:web:88733f7ecea18d24221d9c"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
