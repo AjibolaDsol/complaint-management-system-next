62ab39cae33a0c56de81ca3a29dd49b919af8153
AjibolaDsol <ajibolaDsol@gmail.com>
2022-07-24 01:24:01 +0100

    added some environment changes

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCRUvgQmV98d4XFFVUy-Nc7YGZUjz4COPI",
-  authDomain: "complaint-management-sys-584e9.firebaseapp.com",
-  projectId: "complaint-management-sys-584e9",
-  storageBucket: "complaint-management-sys-584e9.appspot.com",
-  messagingSenderId: "88164443797",
-  appId: "1:88164443797:web:88733f7ecea18d24221d9c"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID 
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
